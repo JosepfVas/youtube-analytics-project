843ae92a6c920ff5d7d3af09f2144767b4d9bb36
Josef Vas <yovzhi@icloud.com>
2024-01-03 15:14:36 +0100

    adding homeword 1

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,7 @@
+from googleapiclient.discovery import build
+import json
+import os
+
 
 
 class Channel:
@@ -5,8 +9,17 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.api_key: str = os.getenv('YOUTUBE-API')
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_data = self.youtube.channels().list(id=self.channel_id,part='snippet,statistics').execute()
+        print(json.dumps(channel_data, indent=2))
+
+
+
+
+
+
