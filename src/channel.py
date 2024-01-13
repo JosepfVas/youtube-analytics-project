c5bfcee93f64422b12286a41ff94e6b54117f9a0
Josef Vas <yovzhi@icloud.com>
2024-01-13 23:49:11 +0100

    addint homework 3

--- src/channel.py
+++ src/channel.py
@@ -47,6 +47,28 @@ class Channel:
         with open(filename, 'w', encoding='utf-8') as file:
             json.dump(channel_data, file, ensure_ascii=False, indent=2)
 
+    def __str__(self):
+        return f'{self.title}("{self.url}")'
+
+    def __add__(self, other):
+        self.subscriber_count += other.subscriber_count
+
+    def __sub__(self, other):
+        self.subscriber_count -= other.subscriber_count
+        other.subscriber_count -= self.subscriber_count
+
+    def __gt__(self, other):
+        self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        self.subscriber_count >= other.subscriber_count
+
+    def __lt__(self, other):
+        self.subscriber_count > other.subscriber_count
+
+    def __le__(self, other):
+        self.subscriber_count >= other.subscriber_count
+
 
 
 
