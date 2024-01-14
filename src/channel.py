d44b527ac854c128acd70e1cfabd51724c230c51
Josef Vas <yovzhi@icloud.com>
2024-01-14 15:26:17 +0100

    adding some changes

--- src/channel.py
+++ src/channel.py
@@ -7,7 +7,7 @@ class Channel:
     API_KEY = 'AIzaSyCcewIqkMw786Rey_TH2XGxGNlFVgg2_Eg'
 
     def __init__(self, channel_id: str) -> None:
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.api_key = self.API_KEY
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
@@ -16,13 +16,13 @@ class Channel:
         self.id = channel_data.get('id')
         self.title = channel_data.get('snippet', {}).get('title')
         self.description = channel_data.get('snippet', {}).get('description')
-        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
         self.subscriber_count = int(channel_data.get('statistics', {}).get('subscriberCount', 0))
         self.video_count = int(channel_data.get('statistics', {}).get('videoCount', 0))
         self.view_count = int(channel_data.get('statistics', {}).get('viewCount', 0))
 
     def print_info(self) -> None:
-        channel_data = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel_data = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel_data, indent=2))
 
     @classmethod
@@ -30,7 +30,7 @@ class Channel:
         return build('youtube', 'v3', developerKey=cls.API_KEY)
 
     def get_channel_data(self) -> dict:
-            request = self.youtube.channels().list(id=self.channel_id,part='snippet,statistics')
+            request = self.youtube.channels().list(id=self.__channel_id,part='snippet,statistics')
             response = request.execute()
             return response['items'][0] if response.get('items') else {}
 
